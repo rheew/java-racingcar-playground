de80ac8b0469f764c7eaa9627de1a7bc2ce9e068
rheew <47098896+rheew@users.noreply.github.com>
2022-06-20 16:06:59 +0900

    feat: 4 이상일 경우전진 기능

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,7 +2,9 @@ package racingcar;
 
 public class Car {
 
-    private final int MAX_LENGTH = 5;
+    private static final int MIN_FORWARD_SCORE = 4;
+    private static final int MAX_NAME_LENGTH = 5;
+    private int position;
 
     public Car(String name) {
         if (validationName(name)) {
@@ -11,6 +13,21 @@ public class Car {
     }
 
     private boolean validationName(String name) {
-        return name.length() > MAX_LENGTH;
+        return name.length() > MAX_NAME_LENGTH;
+    }
+
+    public void forward(int score) {
+        if (isForward(score)) {
+            return;
+        }
+        position += 1;
+    }
+
+    private boolean isForward(int score) {
+        return score < MIN_FORWARD_SCORE;
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
